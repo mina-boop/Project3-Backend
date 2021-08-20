5411f2f01618f7ca18e2594e15c36f569f36432b
abdou91140 <abdou91140@DESKTOP-JDPMG1D.localdomain>
2021-08-20 10:02:53 +0200

    Memes routes begin

--- routes/memes.js
+++ routes/memes.js
@@ -6,19 +6,32 @@ const Meme = require("../models/Meme");
 
 //Post Items infos:
 
-router.post("/create", (req, res, next) => {
-  Meme.create(req.body)
-  .then((memeDocument)=>{res.status(201).json(memeDocument);
-  })
-  .catch((e)=>console.log(e))
+router.post("/create", requireAuth, (req, res, next) => {
+  /*   console.log(req.body);
+    if (req.file) {
+      req.body.image = req.file.path;
+    } else {
+      req.body.image = undefined;
+    } */
+  User.findById(req.session.currentUser).then((userDocument) => {
+    Meme.create(userDocument._id, req.body)
+      .then((memeDocument) => {
+        res.status(201).json(memeDocument);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+
+  }).catch()
+
 });
 
-router.patch("/:id", requireAuth, (req,res,next)=>{
-  Meme.findByIdAndUpdate(req.params.id, meme, {new:true})
-  .then((updateDocument)=>{
-    return res.status(200).json(updateDocument);
-  })
-  .catch((e)=>console.log(e))
+router.patch("/:id", requireAuth, (req, res, next) => {
+  Meme.findByIdAndUpdate(req.params.id, meme, { new: true })
+    .then((updateDocument) => {
+      return res.status(200).json(updateDocument);
+    })
+    .catch((e) => console.log(e))
 })
 
 
@@ -26,4 +39,4 @@ router.patch("/:id", requireAuth, (req,res,next)=>{
 
 
 
-module.exports = Meme;
+module.exports = router;
