4f0dce4cac9e05aff7bcfcd8f9644ecac1415f11
abdou91140 <abdou91140@DESKTOP-JDPMG1D.localdomain>
2021-08-20 12:13:50 +0200

    create memes

--- routes/memes.js
+++ routes/memes.js
@@ -6,8 +6,10 @@ const Meme = require("../models/Meme");
 
 //Post Meme from the current user: OK
 router.post("/create", requireAuth, (req, res, next) => {
+  //add cuurentUser ObjectId
   User.findById(req.session.currentUser).then((userDocument) => {
-    Meme.create(userDocument._id, req.body)
+    const { caption1, caption2 } = req.body
+    Meme.create({ creator: userDocument, caption1: caption1, caption2: caption2 })
       .then((memeDocument) => {
         res.status(201).json(memeDocument);
       })
@@ -21,9 +23,10 @@ router.post("/create", requireAuth, (req, res, next) => {
 router.patch("/:id", requireAuth, (req, res, next) => {
   User.findById(req.session.currentUser).then((userDocument) => {
     // Only the current user can modify an meme
-    /*     if (userDocument._id !== req.session.currentUser._id) {
-          return res.status(400).json({ message: "Invalid credentials" });
-        } */
+
+    if (userDocument._id !== req.session.currentUser._id) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
     Meme.findByIdAndUpdate(req.params.id, req.body, { new: true })
       .then((memeUpdatedDocument) => {
         res.status(201).json(memeUpdatedDocument);
@@ -34,28 +37,5 @@ router.patch("/:id", requireAuth, (req, res, next) => {
   }).catch()
 
 })
-<<<<<<< HEAD
-=======
-//OK delete sur Postman
-router.delete("/:id", requireAuth, (req,res,next)=>{
-  Meme.findByIdAndDelete(req.params.id)
-  .then(()=> {return res.status(204);
-  })
-  .catch((error)=>console.log(error))
-})
-
-router.get("/:memeId", (req,res,next)=>{
-Meme.findById(req.params.id)
-.then((memeDocument)=>{
-  if(!memeDocument){
-  return res.status(404).json({message:"Meme not found!"});
-  } return res.status(204).json(memeDocument);
-})
-.catch((error)=>console.log(error))
-
-})
-
-
->>>>>>> 3bb3c7695ac825d7eba71373ec88ac2aeb7e5439
 
 module.exports = router;
