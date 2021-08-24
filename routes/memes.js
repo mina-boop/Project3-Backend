456fd15dcbdc6c8778f5fa96d4fddd78dbb27741
MinaTough <mina.toughrani@gmail.com>
2021-08-24 18:34:33 +0200

    i

--- routes/memes.js
+++ routes/memes.js
@@ -4,20 +4,27 @@ const requireAuth = require("../middlewares/requireAuth");
 const upload = require("../config/cloudinary");
 const router = express.Router();
 const Meme = require("../models/Meme");
-
 //Post Meme from the current user: OK
-router.post("/create", requireAuth, upload.single("memeimage"), (req, res, next) => {
-  //add cuurentUser ObjectId
-  
-  User.findById(req.session.currentUser).then((userDocument) => {
-    const newMeme = req.body
-    if (req.file) {
-      newMeme.memeimage = req.file.path; //  ProfileImage key added to req.body
-    }
-    console.log(newMeme)
-    Meme.create({ creator: userDocument._id, ...newMeme })
-      .then((memeDocument) => {
-        res.status(201).json(memeDocument);
+router.post(
+  "/create",
+  requireAuth,
+  upload.single("memeimage"),
+  (req, res, next) => {
+    //add cuurentUser ObjectId
+    User.findById(req.session.currentUser)
+      .then((userDocument) => {
+        const newMeme = req.body;
+        if (req.file) {
+          newMeme.memeimage = req.file.path; //  ProfileImage key added to req.body
+        }
+        console.log(newMeme);
+        Meme.create({ creator: userDocument._id, ...newMeme })
+          .then((memeDocument) => {
+            res.status(201).json(memeDocument);
+          })
+          .catch((error) => {
+            res.status(500).json(error);
+          });
       })
       .catch();
   }
@@ -27,7 +34,6 @@ router.patch("/:id", requireAuth, (req, res, next) => {
   User.findById(req.session.currentUser)
     .then((userDocument) => {
       // Only the current user can modify an meme
-
       if (userDocument._id !== req.session.currentUser._id) {
         return res.status(400).json({ message: "Invalid credentials" });
       }
@@ -49,19 +55,17 @@ router.delete("/:id", requireAuth, (req, res, next) => {
     })
     .catch((error) => console.log(error));
 });
-
-
 router.get("/all", (req, res, next) => {
-  Meme.find().populate("creator")
+  Meme.find()
+    .populate("creator")
     .then((memeDocument) => {
-      res.status(201).json(memeDocument)
+      res.status(201).json(memeDocument);
     })
-    .catch((error) => console.log(error))
-})
-
-
+    .catch((error) => console.log(error));
+});
 router.get("/:memeId", (req, res, next) => {
-  Meme.findById(req.params.memeId).populate("creator")
+  Meme.findById(req.params.memeId)
+    .populate("creator")
     .then((memeDocument) => {
       if (!memeDocument) {
         return res.status(404).json({ message: "Meme not found!" });
@@ -70,7 +74,4 @@ router.get("/:memeId", (req, res, next) => {
     })
     .catch((error) => console.log(error));
 });
-
-
-
 module.exports = router;
